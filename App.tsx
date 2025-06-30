2a1413ac87aceff35da9bc5bb2a1dae477a7efa0
vietanhhappy <vietanhhappy99@gmail.com>
2025-06-30 16:09:25 +0700

    video 30

--- App.tsx
+++ App.tsx
@@ -4,13 +4,25 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{fontSize: 40, color: "red"}}>Việt Anh!...</Text>
+      <Text style={{fontSize: 20}}>Hello world</Text>
+      <Text style={styles.text}>View text</Text>
+      <Text style={styles.text}>Hoidanit
+        <Text style={styles.textGreen} >Test 123</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    color: "red"
+  },
+  textGreen: {
+    color: "green"
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
