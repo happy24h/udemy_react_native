2912a58c877d799c60d1ddfa29fcf8acc348e44a
vietanhhappy <vietanhhappy99@gmail.com>
2025-07-12 16:43:25 +0700

    #47 passing parameters to routes

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { View, Text, Button } from "react-native";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { NavigationContainer, useNavigation, useRoute } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 function HomeScreen({ navigation }: any) {
@@ -12,30 +12,39 @@ function HomeScreen({ navigation }: any) {
       <View style={{ marginVertical: 10 }}>
         <Button
           title="Go to Details"
-          onPress={() => navigation.navigate("Details")}
+          onPress={() =>
+            navigation.navigate("Details", { userId: 1, name: "Eric" })
+          }
         />
       </View>
       <View style={{ marginVertical: 10 }}>
         <Button
           title="GO USER ID = 1"
-          onPress={() => navigation.navigate("Details")}
+          onPress={() =>
+            navigation.navigate("Details", { userId: 2, name: "Viet Anh" })
+          }
         />
       </View>
       <View style={{ marginVertical: 10 }}>
         <Button
           title="GO USER ID = 2"
-          onPress={() => navigation.navigate("Details")}
+          onPress={() =>
+            navigation.navigate("Details", { userId: 3, name: "Văn Huy" })
+          }
         />
       </View>
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen(props: any) {
   const navigation: any = useNavigation();
+  const route: any = useRoute();
+  console.log('check route >>>', route.params);
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>User id = ????</Text>
+      <Text>User id = {route.params.userId}</Text>
+      <Text>User name = {route.params.name}</Text>
       <View style={{ marginVertical: 10 }}>
         <Button
           title="Go to Details... again"
